675f7a7615bcb7bc1eb196297e7971eab1f815e6
Luiz Eduardo <luizeduardokowalski@gmail.com>
2017-06-10 15:04:34 +0200

    Script changes

--- run-helpers.sh
+++ run-helpers.sh
@@ -1,5 +1,5 @@
-docker rm rabbitmq3;
-docker run -d --hostname rabbitmq -p 5672:5672 -p 15672:15672 --name rabbitmq3 rabbitmq:3-management;
+docker stop rabbitmq3 eureka;
+docker rm rabbitmq3 eureka;
 
-docker rm eureka;
+docker run -d --hostname rabbitmq -p 5672:5672 -p 15672:15672 --name rabbitmq3 rabbitmq:3-management;
 docker run -d --hostname eureka -p 8761:8761 --name eureka luizkowalski/eureka-server
